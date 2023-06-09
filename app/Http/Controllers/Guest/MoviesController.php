15295ea34a6f7accb7dec9df34fa064bf4ccc0be
gluke0 <milanesigianluca@gmail.com>
2023-06-09 15:46:36 +0200

    model Movie ok

--- app/Http/Controllers/Guest/MoviesController.php
+++ app/Http/Controllers/Guest/MoviesController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MoviesController extends Controller
 {
     public function getMovies(){
-        return view('pages.movies');
+
+        $movies = Movie::All();
+
+        dd($movies);
+        
+        return view('pages.movies', compact('movies'));
     }
 }
